e24333c7fa4a6d521771d98085710be92f0edaeb
Krishana <Krishana@LAPTOP-URO1MVAN>
2026-08-26 13:21:13 +0530

    first commit

--- Java_programs/src/org/tnsif/acc/c2tc/manager_rules/KIA_Rules.java
+++ Java_programs/src/org/tnsif/acc/c2tc/manager_rules/KIA_Rules.java
@@ -1,4 +1,4 @@
-package Manager_Rules;
+package org.tnsif.acc.c2tc.manager_rules;
 
 public class KIA_Rules {
 
---
+++ Java_programs/src/org/tnsif/acc/c2tc/sr_developer/KIA_internal.java
@@ -0,0 +1,15 @@
+package org.tnsif.acc.c2tc.sr_developer;
+import org.tnsif.acc.c2tc.jr_developer.*;
+
+public class KIA_internal {
+
+	public static void main(String[] args) {
+		org.tnsif.acc.c2tc.jr_developer.KIA_AirBags ob=new org.tnsif.acc.c2tc.jr_developer.KIA_AirBags();
+		ob.display_Air();
+		org.tnsif.acc.c2tc.jr_developer.KIA_SpeedLimit ob1=new org.tnsif.acc.c2tc.jr_developer.KIA_SpeedLimit();
+		ob1.display_speed();
+		
+
+	}
+
+}
